31399767347462ce39d448854b963e41a3253d4e
Richard Luo <cdominant7@gmail.com>
2011-01-05 17:11:13 +0800

    add for indexing just

---
+++ CSCOPE_JUST_DO_INDEX.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+################################################################
+# file: CSCOPE_JUST_DO_INDEX.sh
+# author: Richard Luo
+# date: 2011/01/05 09:09:31
+################################################################
+
+LIST_FILE=cscope.files
+DATABASE_FILE=cscope.out
+
+if [ ! -f $LIST_FILE ]; then
+    echo "there is no list file: $LIST_FILE"
+    exit 100
+fi
+
+cscope -b -i $LIST_FILE -f $DATABASE_FILE
