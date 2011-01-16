f7d86b6e88ee0238c24480f8b975bd88cb0335a1
Richard Luo <cdominant7@gmail.com>
2011-01-16 13:57:57 +0800

    how to build nexus one

---
+++ BuildNexus1.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+################################################################
+# file: build_nexus_one.sh
+# author: Richard Luo
+# date: 2010/12/17 08:56:47
+################################################################
+
+
+export ANDROID_PRODUCT_OUT=out/target/product/htc/passion
+
+. build/envsetup.sh
+
+lunch full_passion-userdebug
+
+cd device/htc/passion  &&  sh extract-files.sh
+
+make -j4 showcommands
+
+
