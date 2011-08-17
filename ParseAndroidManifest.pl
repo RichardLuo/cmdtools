08c725eeea2576789d63f5417108cfce5c8b7fb4
Richard Luo <cdominant7@gmail.com>
2011-08-17 13:41:23 +0800

    add ParseAndroidManifest.pl

---
+++ ParseAndroidManifest.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+################################################################
+# file: ParseAndroidManifest.pl
+# author: Richard Luo
+# date:   2011-08-17                                  
+################################################################
+use strict;
+use warnings;
+
+use strict;
+use warnings;
+use Getopt::Std;
+use File::Basename;
+
+while (<>) {
+    if (s/package\s*=\s*"(\w+\.\w+.*)"/$1/) {
+        print $1 . "\n";
+        exit 0;
+    }
+}
+exit 100
+
+
