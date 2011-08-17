2b6525e92c16f2e4c65b13edb3b970449c683d13
Richard Luo <cdominant7@gmail.com>
2011-08-17 14:08:13 +0800

    Modified     CCleanAndroidJava.sh      add files to clean

--- CCleanAndroidJava.sh
+++ CCleanAndroidJava.sh
@@ -5,6 +5,8 @@
 # 2008-11-11
 ################################################################
 
-rm -rf assets bin/ libs/ gen/ default.properties local.properties build.xml cscope.* .class* .projec* obj
+rm -rf assets bin/ libs/ gen/ default.properties \
+    local.properties build.xml cscope.* .class* \
+    .projec* obj  proguard.cfg _index.html build.properties
 
 
