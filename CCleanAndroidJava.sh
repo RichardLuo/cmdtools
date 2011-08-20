ffdffe929360a022d7a94be4851e7be047d11061
Richard Luo <cdominant7@gmail.com>
2011-08-20 11:22:06 +0800

    the file 'default.properties' should not be cleaned

--- CCleanAndroidJava.sh
+++ CCleanAndroidJava.sh
@@ -5,7 +5,7 @@
 # 2008-11-11
 ################################################################
 
-rm -rf assets bin/ libs/ gen/ default.properties \
+rm -rf assets bin/ libs/ gen/ \
     local.properties build.xml cscope.* .class* \
     .projec* obj  proguard.cfg _index.html build.properties
 
