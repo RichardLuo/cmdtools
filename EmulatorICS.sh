eb343f94b842b8ed0ad4149c1a6348752555931d
Richard Luo <cdominant7@gmail.com>
2012-04-11 10:58:17 +0800

    add two files for start android emulator

---
+++ EmulatorICS.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+################################################################
+# file: EmulatorGinger.sh
+# author: Richard Luo
+# date: 2010/12/28 12:32:09
+################################################################
+
+sysdir=/data/android-ics/out/target/product/generic
+# avd=Kitty4.0
+avd=ICS4
+partition_size_M=256
+
+         # -sdcard $HOME/scard.img                \
+
+
+emulator -system ${sysdir}/system.img           \
+         -datadir ${sysdir}                     \
+         -avd ${avd}                            \
+         -partition-size ${partition_size_M} &
+
+
