06f05b29e20a13977c381ffd1d7e9c8b6ff02112
Richard Luo <cdominant7@gmail.com>
2012-04-16 19:17:44 +0800

    add NameSpaceRecover.sh to convert NAMESPAE_START macro to normal cpp code

---
+++ NameSpaceRecover.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+################################################################
+# file:   Name.sh
+# author: Richard Luo
+# date:   2012-02-19 12:43:20
+################################################################
+
+perl -p -i -e 's/NAMESPACE_START\((\w+)\).*/namespace $1 \{/g' $1
+perl -p -i -e 's/NAMESPACE_END\((\w+)\).*/} \/\/ end of namespace $1/g' $1
