10ee10721a2375441261ef1b5e4fde6176ae9a18
Richard Luo <cdominant7@gmail.com>
2012-04-16 19:16:44 +0800

    it worked for make cnx's cpio image, using 30800000 is ok, not very clear why 30800000 does work

--- MK_IMAGE.sh
+++ MK_IMAGE.sh
@@ -10,10 +10,18 @@ if [ ! -f $1 ]; then
     exit 100
 fi
 
+ADDRESS=0x30800000
+if [ "X$2" != "X" ];then
+    ADDRESS=$2
+fi
+
+## MKIMAGE_TOOL=/usr/bin/mkimage
+MKIMAGE_TOOL=/home/richard/bin/mkimage
+
 #  mkimage -A arm -O linux -T ramdisk -C none -a 0x30800000 -n "ramdisk" -d ramdisk.img ramdisk-uboot.img
-if mkimage -A arm -O linux -T ramdisk -C none -a 0x30800000 -n "ramdisk" -d $1 $1.img; then
-    echo "mkimage ok, output the $1.img file here!"
+if ${MKIMAGE_TOOL} -A arm -O linux -T ramdisk -C none -a ${ADDRESS} -n "ramdisk" -d $1 $1.img; then
+    echo "${MKIMAGE_TOOL} ok, output the $1.img file here!"
 else
-    echo "mkimage failed!!!"
+    echo "${MKIMAGE_TOOL} failed!!!"
 fi
 
