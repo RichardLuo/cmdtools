ebab5b2136155108758003e47781d0622993b1ec
Richard Luo <cdominant7@gmail.com>
2012-04-18 10:39:07 +0800

    add FirstGitCommit.sh to help setup git repo

---
+++ FirstGitCommit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+################################################################
+# file:   FirstGitCommit.sh
+# author: Richard Luo
+# date:   2012-04-18 10:33:48
+################################################################
+
+if [ -d .git ]; then
+    echo ".git already exist, please confirm it!";
+    exit 10;
+fi
+
+if git init && git add . && git commit -m'first commit' .; then
+    echo "================================================================";
+    echo "                setup git ok!";
+    echo "================================================================";
+fi
+
+
